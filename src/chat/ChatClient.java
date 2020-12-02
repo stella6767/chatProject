8f3530b93fdf667b0b2ac5c11531ac208adc52ee
stella6767 <alsrb9434@gmail.com>
2020-12-02 18:21:28 +0900

    소켓 통신 다대다 채팅프로그램 구현

---
+++ src/chat/ChatClient.java
@@ -0,0 +1,178 @@
+package chat;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.ScrollPane;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+public class ChatClient extends JFrame {
+
+	private final static String TAG = "ChatClient: ";
+	private ChatClient chatClient = this;
+
+	private static final int PORT = 10000;
+
+	private JButton btnConnect, btnSend; // 앞에 공통적인 이름을 넣는 게 좋음, 찾을 때 편함
+	private JTextField tfHost, tfChat;
+	private JTextArea taChatList;
+	private ScrollPane scrollPane;
+	private JPanel topPanel, bottomPanel;
+	private Socket socket;
+	private PrintWriter writer;
+	private BufferedReader reader;
+	
+
+	public ChatClient() {
+		init();
+		setting();
+		batch();
+		listener();
+
+		setVisible(true);
+
+	}
+
+	private void init() {
+		btnConnect = new JButton("connect");
+		btnSend = new JButton("send");
+		tfHost = new JTextField("127.0.0.1", 20);
+		tfChat = new JTextField(20);
+		taChatList = new JTextArea(10, 30); // row,column
+		scrollPane = new ScrollPane();
+		topPanel = new JPanel();
+		bottomPanel = new JPanel();
+	}
+
+	private void setting() {
+		setTitle("채팅 다대다 클라이언트");
+		setSize(400, 350);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		taChatList.setBackground(Color.ORANGE);
+		taChatList.setForeground(Color.BLUE);
+	}
+
+	private void batch() {
+		topPanel.add(tfHost);
+		topPanel.add(btnConnect);
+		bottomPanel.add(tfChat);
+		bottomPanel.add(btnSend);
+		scrollPane.add(taChatList);
+
+		add(topPanel, BorderLayout.NORTH);
+		add(scrollPane, BorderLayout.CENTER);
+		add(bottomPanel, BorderLayout.SOUTH);
+	}
+
+	private void listener() {
+		btnConnect.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				connect();
+
+			}
+		});
+
+		btnSend.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				send();
+			}
+		});
+		
+		
+		addWindowListener(new WindowAdapter() {
+			
+			@Override
+			public void windowClosing(WindowEvent e) {
+				writer.println(Protocol.Exit);
+				writer.flush();
+			}
+		});
+
+	}
+
+	private void send() {
+
+		String chat = tfChat.getText();
+		// 1번 taChatList 뿌리기
+		taChatList.append("[내 메시지] " + chat + "\n"); // 추가
+		// 2번 서버로 전송
+		writer.println(chat);
+		writer.flush();
+
+		// 3번 tfchat 비우기
+		tfChat.setText(null);
+
+	}
+
+	private void connect() {
+
+		String host = tfHost.getText();
+		try {
+			
+			taChatList.append("ALL: 치고 글자 입력하세요.(프로토콜 때문) " + "\n");
+			socket = new Socket(host, PORT);
+			reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			writer = new PrintWriter(socket.getOutputStream(), true);
+			ReaderThread rt = new ReaderThread();
+			rt.start();
+			taChatList.append("서버에 연결하였습니다." +"\n");
+
+		} catch (Exception e1) {
+			System.out.println(TAG + "서버 연결 에러" + e1.getMessage());
+			taChatList.append("서버 연결 에러"+"\n");
+		}
+
+	}
+
+	class ReaderThread extends Thread {
+		
+		
+		
+		// while을 돌면서 서버로부터 메시지를 받아서 taChatList에 뿌리기
+		@Override
+		public void run() {
+			String input = null;
+
+			try {
+			
+				while ((input = reader.readLine()) != null) {
+					taChatList.append(input + "\n");
+
+				}
+
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+
+	}
+
+	public static void main(String[] args) {
+
+			new ChatClient();
+		
+	}
+}
---
+++ src/chat/ChatServer.java
@@ -0,0 +1,119 @@
+package chat;
+
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Vector;
+
+public class ChatServer {
+
+	private static final String TAG = "ChatServer: ";
+	private ServerSocket serverSocket;
+	private Vector<ClientInfo> vc; // 연결된 클라이언트 소켓을 담는 컬렉션
+	static FileWriter fileWriter;
+	
+	
+	
+	public ChatServer() {
+		
+		
+		
+		try {
+			serverSocket = new ServerSocket(10000);
+			System.out.println(TAG + "클라이언트 연결 대기중...");
+			vc = new Vector<>();
+			// 메인쓰레드의 역할
+			while (true) {
+				Socket socket = serverSocket.accept(); // 클라이언트 연결 대기
+				ClientInfo clientInfo = new ClientInfo(socket);
+				clientInfo.start();
+				vc.add(clientInfo);
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	class ClientInfo extends Thread {
+
+		Socket socket;
+		BufferedReader reader;
+		PrintWriter writer; // BufferedWriter와 다른 점은 내려쓰기 함수를 지원+객체 만들기 편안
+		
+
+		public ClientInfo(Socket socket) {
+			this.socket = socket;
+
+			try {
+				reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				writer = new PrintWriter(socket.getOutputStream());
+				System.out.println("연결 성공");
+		
+			} catch (Exception e) {
+				System.out.println("서버 연결 실패: " + e.getMessage());
+			}
+
+		}
+
+		// 역할: 클라이언트로 부터 받은 메시지를 모든 클라이언트에게 재전송
+		@Override
+		public void run() {
+			String line =null;
+			String ChatLog  ="";
+			
+			try {
+				fileWriter = new FileWriter("D:\\workspace/채팅로그.txt",true);
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			
+			try {
+				while ((line = reader.readLine())!= null) {								
+						router(line);
+						fileWriter.write(line,0,line.length());
+						fileWriter.write("\r\n",0,2);
+						fileWriter.flush();
+						if (line.contains(Protocol.Exit)) {
+							break;
+							
+						}
+				}
+				
+				fileWriter.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+		}
+		
+		
+		public void router(String line) {
+			for (int i = 0; i < vc.size(); i++) {
+				if(vc.get(i) != this && line.contains(Protocol.ALL)) {
+					vc.get(i).writer.println("[클라이언트" + this.getName() +"] " + line);
+					vc.get(i).writer.flush();									
+				}else if(!line.contains(Protocol.ALL) && vc.get(i) == this){
+					vc.get(i).writer.println("프로토콜 입력유무를 확인해주세요.");
+					vc.get(i).writer.flush();
+				}
+			}
+			
+			
+		}
+		
+		
+
+	}
+	
+	
+	public static void main(String[] args) {
+		new ChatServer();
+	}
+}
